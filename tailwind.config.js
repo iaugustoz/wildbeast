02bd23861c8a18e15e7153d44966dc0bf5282594
Igor Augusto <igoraugustobz@gmail.com>
2023-08-22 20:16:27 -0300

    feat: adiciona seção sidenav estilizada

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,23 @@ module.exports = {
     './**/*.html',
     '.**/*.html',
   ],  theme: {
-    extend: {
+    extend: {  
+    boxShadow: {
+      'sm': '5px 5px 15px 0px rgba(44, 44, 44, 0.15)',
+    },
+    spacing: {
+      '20px': '20px',
+      '10px': '10px',
+      '30px': '30px',
+    },
+    colors: {
+      'lilac-900': '#8844EE',
+      'lilac-600':'#B07DFB',
+      'gray-25': '#FAFBFC',
+    },
+      backgroundImage: {
+        'lilac-gradient': 'linear-gradient(135deg, #8844EE, #B07DFB)',
+      },
       fontFamily: {
         'vollkorn': ['Vollkorn', 'Georgia', 'Serif'],
       },  
